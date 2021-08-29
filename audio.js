06bfdb77e20afc40b51fc8654181e76412364db0
Kimbatt <kimbatt@citromail.hu>
2021-08-29 21:42:37 +0200

    Add audio stuff

---
+++ audio.js
@@ -0,0 +1,379 @@
+
+/**
+ * @type {AudioContext}
+ */
+let actx;
+
+/**
+ * @type {AudioBuffer}
+ */
+let noiseBuffer;
+
+/**
+ * @type {PeriodicWave}
+ */
+let bassWave;
+
+/**
+ *
+ * @param {AudioContext} audioContext
+ * @param {number} length
+ * @param {(t: number) => number} generator
+ */
+function CreateBufferWithData(audioContext, length, generator)
+{
+    let buffer = audioContext.createBuffer(1, audioContext.sampleRate * length | 0, audioContext.sampleRate);
+    let data = buffer.getChannelData(0);
+    for (let i = 0; i < data.length; ++i)
+    {
+        data[i] = generator(i / audioContext.sampleRate);
+    }
+
+    return buffer;
+}
+
+let once = false;
+function Init()
+{
+    if (once)
+        return;
+    once = true;
+    actx = new AudioContext();
+
+    let rng = mulberry32(0);
+    noiseBuffer = CreateBufferWithData(actx, 1, () => rng() * 2 - 1);
+
+    let coeffs = [0, 1, 0.4, 0.2, 0.3, 0.1];
+    bassWave = actx.createPeriodicWave(coeffs, [0, 1, 0, 0.2, 0.5, 0.2]);
+
+    Start();
+}
+
+document.addEventListener("click", Init);
+// document.addEventListener("keydown", Init);
+
+/**
+ *
+ * @param {AudioContext} audioContext
+ * @param {AudioBuffer} bufferData
+ * @returns
+ */
+function CreateNodeFromBufferData(audioContext, bufferData)
+{
+    let node = audioContext.createBufferSource();
+    node.buffer = bufferData;
+    return node;
+}
+
+function CreateNoiseNode()
+{
+    let node = CreateNodeFromBufferData(actx, noiseBuffer)
+    node.loop = true;
+    return node;
+}
+
+/**
+ * @param {number} octave
+ * @param {number} note
+ */
+function NoteToFrequency(octave, note)
+{
+    return 2 ** (Math.log2(440) + octave - 4 + (note - 9) / 12);
+}
+
+
+/**
+ *
+ * @param {number} frequency
+ * @param {number} volume
+ * @param {number} when
+ * @param {number} duration
+ * @param {number} fadeInDuration
+ * @param {number} fadeOutDuration
+ * @param {number} Q
+ * @param {OscillatorType} wave
+ */
+function PlaySound(frequency, volume, when, duration, fadeInDuration, fadeOutDuration, Q, wave = "square")
+{
+    let oscillator = actx.createOscillator();
+    let gain = actx.createGain();
+    let filter = actx.createBiquadFilter();
+    // let filter2 = actx.createBiquadFilter();
+
+    oscillator.frequency.value = frequency;
+
+    gain.gain.value = 0;
+    let time = when;
+    gain.gain.linearRampToValueAtTime(0, time);
+    time += fadeInDuration;
+    gain.gain.linearRampToValueAtTime(volume, time);
+    time += duration;
+    gain.gain.linearRampToValueAtTime(volume, time);
+    time += fadeOutDuration;
+    gain.gain.linearRampToValueAtTime(0, time);
+
+    filter.type = "bandpass";
+    filter.Q.value = Q;
+    filter.frequency.value = frequency;
+
+    // filter2.type = "highpass";
+    // filter2.frequency.value = frequency * 0.5;
+    // filter2.Q.value = -10;
+
+
+    // oscillator.connect(gain);
+    // gain.connect(filter);
+    // filter.connect(filter2);
+    // filter2.connect(actx.destination);
+
+
+    oscillator.connect(gain);
+    gain.connect(filter);
+    filter.connect(actx.destination);
+
+    oscillator.setPeriodicWave(bassWave);
+    oscillator.start(when);
+    oscillator.stop(time);
+}
+
+/**
+ * @param {number} volume
+ * @param {number} when
+ * @param {AudioBufferSourceNode | OscillatorNode} sourceNode
+ * @param {boolean} filter
+ * @param {number} filterFrequency
+ * @param {number} Q
+ */
+function Drum(volume, when, sourceNode, filter, filterFrequency, Q, fadeInDuration = 0.01, fadeOutDuration = 0.1, duration = 0)
+{
+    let gainNode = actx.createGain();
+    let filterNode = actx.createBiquadFilter();
+
+    gainNode.gain.value = 0;
+    let time = when;
+    gainNode.gain.linearRampToValueAtTime(0, time);
+    time += fadeInDuration;
+    gainNode.gain.linearRampToValueAtTime(volume, time);
+    time += duration;
+    gainNode.gain.linearRampToValueAtTime(volume, time);
+    time += fadeOutDuration;
+    gainNode.gain.linearRampToValueAtTime(0, time);
+
+    filterNode.type = filter ? "bandpass" : "allpass";
+    filterNode.frequency.value = filterFrequency;
+    filterNode.Q.value = Q;
+
+    sourceNode.connect(gainNode);
+    gainNode.connect(filterNode);
+    filterNode.connect(actx.destination);
+
+    sourceNode.start(when);
+    sourceNode.stop(time);
+}
+
+function Hat(when)
+{
+    Drum(0.4, when, CreateNoiseNode(), true, 10000, 3);
+}
+
+function Hat2(when)
+{
+    Drum(0.4, when, CreateNoiseNode(), true, 10000, 1, 0.02, 0.06);
+}
+
+function Snare(when)
+{
+    Drum(0.15, when, CreateNoiseNode(), true, 1800, 1, 0.01, 0.05, 0.07);
+}
+
+function Kick(when)
+{
+    let sourceNode = actx.createOscillator();
+    let startFreq = 250;
+    sourceNode.frequency.value = startFreq;
+    sourceNode.frequency.linearRampToValueAtTime(startFreq, when + 0.01);
+    sourceNode.frequency.linearRampToValueAtTime(50, when + 0.03);
+
+    Drum(0.6, when, sourceNode, false, 0, 0, 0.01, 0.05, 0.1);
+}
+
+let duration = 4;
+let scheduleAheadTime = duration + 1;
+let scheduledCount = 0;
+function Start()
+{
+    let start = actx.currentTime + 0.1;
+
+    function PlayEcho(when, octave, note, duration)
+    {
+        let freq = NoteToFrequency(octave, note);
+        let baseVolume = 0.3;
+        PlaySound(freq, baseVolume / 1, when,        duration, 0.01, 0.05, 0.0);
+        // PlaySound(freq, baseVolume / 2, when + 0.25, duration, 0.01, 0.05, 10.0);
+        // PlaySound(freq, baseVolume / 4, when + 0.50, duration, 0.01, 0.05, 20.0);
+        // PlaySound(freq, baseVolume / 4, when + 0.50, duration, 0.01, 0.1, 0.8);
+        // PlaySound(freq, baseVolume / 8, when + 0.75, duration, 0.01, 0.1, 2);
+    }
+
+    let kickActive = true;
+    let snareActive = !true;
+    let hatActive = !true;
+    let hatFn = Hat;
+
+    let rng = mulberry32(0);
+
+    let noteRng;
+
+    let update = () =>
+    {
+        let elapsed = actx.currentTime;
+        let requiredCount = (elapsed + scheduleAheadTime) / duration;
+
+        let half = duration / 2;
+        let quarter = duration / 4;
+        let eight = duration / 8;
+        let sixteenth = duration / 16;
+
+        while (scheduledCount < requiredCount)
+        {
+            let time = start + scheduledCount * duration;
+
+            if (scheduledCount % 4 === 0)
+            {
+                const maxNoteSequenceCount = 10;
+                noteRng = mulberry32(rng() * maxNoteSequenceCount | 0);
+            }
+
+            ++scheduledCount;
+
+            if (kickActive)
+            {
+                for (let i = 0; i < 8; ++i)
+                {
+                    Kick(time + eight * i);
+                }
+            }
+
+            if (snareActive)
+            {
+                for (let i = 0; i < 4; ++i)
+                {
+                    Snare(time + quarter * i + eight);
+                }
+            }
+
+            if (hatActive)
+            {
+                for (let i = 0; i < 8; ++i)
+                {
+                    hatFn(time + eight * (i + 0.5));
+                }
+            }
+
+            if (scheduledCount % 2 === 0)
+            {
+                if (!kickActive || rng() < 0.2)
+                {
+                    kickActive = !kickActive;
+                }
+                else if (!snareActive || rng() < 0.2)
+                {
+                    snareActive = !snareActive;
+                }
+                else if (!hatActive || rng() < 0.2)
+                {
+                    hatActive = !hatActive;
+                }
+                else if (rng() < 0.2)
+                {
+                    hatFn = hatFn === Hat ? Hat2 : Hat;
+                }
+
+                // if (!kickActive && !snareActive && !hatActive)
+                // {
+                //     let random = rng();
+                //     if (random < 0.33)
+                //     {
+                //         kickActive = true;
+                //     }
+                //     else if (random < 0.66)
+                //     {
+                //         snareActive = true;
+                //     }
+                //     else
+                //     {
+                //         hatActive = true;
+                //     }
+                // }
+            }
+
+            const tones = [0, 2, 4, 7, 9];
+            // const tones = [4, 5, 9, 11, 12];
+            const randomTone = () => tones[noteRng() * tones.length | 0] + 3;
+
+            for (let i = 0; i < 8; ++i)
+            {
+                if (noteRng() < 0.5)
+                {
+                    PlayEcho(time + eight * i, 2, randomTone(), sixteenth * 0.5);
+                }
+                else
+                {
+                    PlayEcho(time + sixteenth * (i * 2),     2, randomTone(), sixteenth * 0.5);
+                    PlayEcho(time + sixteenth * (i * 2 + 1), 2, randomTone(), sixteenth * 0.5);
+                }
+            }
+
+            // let noteOffset = 5;
+            // let empty = Symbol();
+            // let timer = 0;
+            // function Play(note, sixteenths = 1)
+            // {
+            //     if (note !== empty)
+            //     {
+            //         PlayEcho(time + timer * sixteenth, 2, note + noteOffset, sixteenth * sixteenths);
+            //     }
+
+            //     timer += sixteenths;
+            // }
+
+            // if (rng() < 0.0)
+            // {
+            //     Play(-3, 2);
+            //     Play(2);
+            //     Play(-3);
+            //     Play(7);
+            //     Play(-3);
+            //     Play(12);
+            //     Play(9);
+            //     Play(-3, 2);
+            //     Play(2);
+            //     Play(-3, 2);
+            //     Play(2);
+            //     Play(4);
+            //     Play(-3);
+            // }
+            // else
+            // {
+            //     Play(4, 2);
+            //     Play(empty, 3);
+            //     Play(4, 0.5);
+            //     Play(2, 0.5);
+            //     Play(empty, 0.5);
+            //     Play(0, 0.5);
+            //     Play(-3, 0.5);
+            //     Play(empty, 0.5);
+            //     Play(0, 2);
+            //     Play(empty, 3);
+            //     Play(2, 0.5);
+            //     Play(0, 0.5);
+            //     Play(empty, 0.5);
+            //     Play(-3, 0.5);
+            //     Play(-5, 0.5);
+            // }
+        }
+    };
+
+    update();
+    setInterval(update, 1379);
+}
--- math.js
+++ math.js
@@ -24,3 +24,17 @@ var falloff = {
     easeIn: t => 1 - Math.cos(t * Math.PI / 2),
     easeOut: t => Math.sin(t * Math.PI / 2)
 };
+
+/**
+ * @param {number} seed
+ */
+function mulberry32(seed)
+{
+    return () =>
+    {
+        let t = seed += 0x6D2B79F5;
+        t = Math.imul(t ^ t >>> 15, t | 1);
+        t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+        return ((t ^ t >>> 14) >>> 0) / (2 ** 32);
+    };
+}
